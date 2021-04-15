1cceffe08f59bd257e04b3922cd59c253b867e76
unknown <hungerforcode@gmail.com>
2021-04-15 20:38:32 +0530

    glyphicon issue fixed

--- resources/views/admin/base.blade.php
+++ resources/views/admin/base.blade.php
@@ -9,7 +9,7 @@
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <!-- Bootstrap 3.3.5 -->
-    <link rel="stylesheet" href="{{ asset('../resources/assets/admin/bootstrap/css/bootstrap.min.css') }}">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" type="text/css" rel="stylesheet">
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
     <!-- Ionicons -->
