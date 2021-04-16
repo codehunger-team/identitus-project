fd0f6b2a8f41ef21ecec6fad8689a9a050e93b94
David Diomede <david@d2creativedesign.com>
2021-04-16 07:04:27 -0700

    spacing

--- resources/views/admin/partials/admin-nav.blade.php
+++ resources/views/admin/partials/admin-nav.blade.php
@@ -9,7 +9,7 @@
         box-shadow: inset -1px 0 0 rgba(0, 0, 0, .1);
     }
 </style>
-<div class="col-md-3 col-lg-2 d-md-block bg-primary sidebar collapse py-4 vh-100" style="width: 280px;">
+<div class="col-md-3 col-lg-2 d-md-block bg-primary sidebar collapse p-4 vh-100" style="width: 280px;">
     <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
         <span class="fs-4">Identitius</span>
     </a>
