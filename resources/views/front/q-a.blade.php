65b6fa07935c169f5741d7da57d0222bf5bdf63e
David Diomede <david@d2creativedesign.com>
2021-04-26 07:08:14 -0700

    QA ontent

--- resources/views/front/q-a.blade.php
+++ resources/views/front/q-a.blade.php
@@ -1,211 +1,138 @@
 @extends('layouts.app')
 @section('content')
-<div class="row mt-6">
-   <div class="col-sm-12">
-      <h1 class="text-theme text-center">Questions &amp; Answers</h1>
-      <div class="separator-3"></div>
-      <hr>
-   </div>
-</div>
-<div class="container my-4 ">
-   <div class="row">
-      <div class="col-lg-8 col-sm-12 mx-auto">
-         <div class="card">
-            <div class="card-body">
-               <p>
-               </p>
-               <p></p>
-               <h3>How the do you pronounce
-                  Identitius?
-               </h3>
-               <p></p>
-               <p>&nbsp;</p>
-               <p></p>
-               <p><strong>/
-                  ahy•den•ti•tee•uhs
-                  /</strong> Yeah, it’s a mouthful at first, but just
-                  say it like this, “identity us.” Now just say
-                  it faster.
-               </p>
-               <p></p>
-               <p>&nbsp;</p>
-               <p></p>
-               <h3>Who controls the domain while the lease in
-                  in effect?
-               </h3>
-               <p></p>
-               <p>&nbsp;</p>
-               <p></p>
-               <p>The domain owner always retains ownership
-                  rights and control of the domains listed on Identitius. However, DNS
-                  instructions are controlled by Identitius. So when you rent or lease a domain
-                  name, you are given full DNS control as long as the terms of the lease
-                  agreement are met.
-               </p>
-               <p></p>
-               <p>&nbsp;</p>
-               <p></p>
-               <h3>How frequently do I need to make
-                  payments?
-               </h3>
-               <p></p>
-               <p>&nbsp;</p>
-               <p></p>
-               <p>We expect to see monthly payments most
-                  often, but payments schedules are very flexible including quarterly, annually,
-                  weekly or what ever is arranged in the lease that you
-                  negotiate.
-               </p>
-               <p></p>
-               <p>&nbsp;</p>
-               <p></p>
-               <h3>What if I have a late
-                  payment?
-               </h3>
-               <p></p>
-               <p>&nbsp;</p>
-               <p></p>
-               <p>The handling of late payments is determined
-                  by the lease agreement. Often a grace period is establish, but this is
-                  determined between you and the domain owner.
-               </p>
-               <p></p>
-               <p>&nbsp;</p>
-               <p></p>
-               <h3>What if the domain was being used for
-                  another website before I rent it? How do I handle that
-                  traffic?
-               </h3>
-               <p></p>
-               <p>&nbsp;</p>
-               <p></p>
-               <p>Just like any commercial real estate space,
-                  a domain may have had a prior occupant. It is up to you how you handle traffic
-                  that “walks in” looking for the prior occupant. We
-                  find that this is usually a good thing because, well, free
-                  traffic.
-               </p>
-               <p></p>
-               <p>&nbsp;</p>
-               <p></p>
-               <h3>But SEO, doesn’t the prior
-                  occupant impact our SEO?
-               </h3>
-               <p></p>
-               <p>&nbsp;</p>
-               <p></p>
-               <p>Sure. But again, unless the prior occupant
-                  “damaged” the property, you should be in a better
-                  position than if you just start from scratch. Every existing backlink helps. In
-                  some cases, this might even make the domain more valuable.
-               </p>
-               <p></p>
-               <p>&nbsp;</p>
-               <p></p>
-               <h3>What if I do a bunch of work developing a
-                  site and decide to change venues?
-               </h3>
-               <p></p>
-               <p>&nbsp;</p>
-               <p></p>
-               <p>Again, think of this like a commercial real
-                  estate lease. You can move your site wherever you want and let your customers
-                  and Google know. You can ask websites with backlinks to your site to make
-                  changes. But in the end, there may be some residual loss
-                  there.
-               </p>
-               <p></p>
-               <p>&nbsp;</p>
-               <p></p>
-               <h3>What if I want to buy the domain?</h3>
-               <p></p>
-               <p>&nbsp;</p>
-               <p></p>
-               <p>To secure our domain lessees, every domain
-                  listed on Identitius includes an option to buy. The option to buy sets price
-                  for you to be able to purchase the domain. The expiration on this option is
-                  usually the end of the domain lease, but may extend further. It will never
-                  expire before the lease ends. This way, domain lessees are
-                  protected.
-               </p>
-               <p></p>
-               <p>&nbsp;</p>
-               <p></p>
-               <h3>What if I don’t want to buy
-                  the domain, but want to keep using it?
-               </h3>
-               <p></p>
-               <p>&nbsp;</p>
-               <p></p>
-               <p>Once a domain lease ends, possession of the
-                  DNS returns to the domain owner. However, prior to the lease ending, you will
-                  be notified and it is usually desirable for both parties to continue the
-                  lease.
-               </p>
-               <p></p>
-               <p>&nbsp;</p>
-               <p></p>
-               <h3>What if there are prior trademarks on the
-                  domain name being leased?
-               </h3>
-               <p></p>
-               <p>&nbsp;</p>
-               <p></p>
-               <p>We ask all domain owners to guarantee that
-                  their domains are free and clear of any trademark, copyright or other IP
-                  infringements. Identitius makes no guarantees in this regard as it such issues
-                  are between the domain owner and the party leasing the
-                  domain.
-               </p>
-               <p></p>
-               <p>&nbsp;</p>
-               <p></p>
-               <h3>Can I generate or otherwise secure
-                  trademarks, copyrights, or other IP using the domain name
-                  leased?
-               </h3>
-               <p></p>
-               <p>&nbsp;</p>
-               <p></p>
-               <p>This is between you and the domain owner.
-                  There may be a “purchase clause” whereby the domain
-                  owner purchases such assets from you at a prearranged price, but
-                  don’t expect much if anything.
-               </p>
-               <p></p>
-               <p>&nbsp;</p>
-               <p></p>
-               <h3>Does the domain I lease come with
-                  trademark, copyright or other intellectual property rights?
-               </h3>
-               <p></p>
-               <p>&nbsp;</p>
-               <p></p>
-               <p>Sometimes, the domains have copyright or
-                  trademark rights that are included, but check with the owner. Identitius makes
-                  no guarantees as these concerns are to be resolved between the domain lessors
-                  and lessees. Please consult with the domain owner and consider an attorney as
-                  well. We have a list of attorney’s that are happy to
-                  help.
-               </p>
-               <p></p>
-               <p>&nbsp;</p>
-               <p></p>
-               <h3>How do I lease my domain on this
-                  market?
-               </h3>
-               <p></p>
-               <p>&nbsp;</p>
-               <p></p>
-               <p>Were happy to bring you on board. Please
-                  refer to our owner portal by clicking here.
-               </p>
-               <p></p>
-               <br>
-               <p></p>
+
+    <div class="row mt-6">
+        <div class="col-sm-12">
+            <h1 class="text-theme text-center">Questions &amp; Answers</h1>
+            <div class="separator-3"></div>
+            <hr>
+        </div>
+    </div>
+    <div class="container my-4 ">
+        <div class="row">
+            <div class="col-lg-8 col-sm-12 mx-auto">
+                <div class="accordion accordion-flush" id="accordionFlushOne">
+                    {{--                    One--}}
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="flush-headingOne">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                    data-bs-target="#flush-collapseOne" aria-expanded="false"
+                                    aria-controls="flush-collapseOne">
+                                What happens if I want to continue leasing when the lease contract ends?
+                            </button>
+                        </h2>
+                        <div id="flush-collapseOne" class="accordion-collapse collapse"
+                             aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+                            <div class="accordion-body">
+                                <p>You’ll be notified prior to the lease ending; it’s usually
+                                desirable for both parties to continue the lease. Once a domain lease ends, possession
+                                of the DNS records return to the domain owner.</p>
+                            </div>
+                        </div>
+                    </div>
+                    {{--                    Two--}}
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="flush-headingTwo">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                    data-bs-target="#flush-collapseTwo" aria-expanded="false"
+                                    aria-controls="flush-collapseTwo">
+                                Can I buy the domain?
+                            </button>
+                        </h2>
+                        <div id="flush-collapseTwo" class="accordion-collapse collapse"
+                             aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
+                            <div class="accordion-body">
+                                <p>Yep, every domain listed on Identitius includes an option to buy
+                                    up front or during the lease period. The expiration on the purchase option typically
+                                    ends when the lease period ends, but it may end sooner or extend further, depending
+                                    upon the contract.</p>
+                            </div>
+                        </div>
+                    </div>
+                    {{--                    Three--}}
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="flush-headingThree">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                    data-bs-target="#flush-collapseThree" aria-expanded="false"
+                                    aria-controls="flush-collapseThree">
+                                How do trademarks, copyrights, or other IP infringements work when leasing a domain?
+                            </button>
+                        </h2>
+                        <div id="flush-collapseThree" class="accordion-collapse collapse"
+                             aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
+                            <div class="accordion-body">
+                                <p>Identitius asks all domain owners to attest that their
+                                    domains are free of any trademark, copyright, or other IP infringements. Identitius
+                                    makes no guarantees in this regard as trademark issues are between the domain owner
+                                    and the party leasing the domain.</p>
+                                <p>Domains may have copyright or trademark rights included. Check with the owner as
+                                    Identitius makes no guarantees as these issues are between the domain owners and the
+                                    party leasing the domain.</p>
+                                <p>It’s always smart to consult with an attorney as well, and we have a list of
+                                    attorneys who are happy to help.</p>
+                            </div>
+                        </div>
+                    </div>
+                    {{--                    Four--}}
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="flush-headingFour">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                    data-bs-target="#flush-collapseFour" aria-expanded="false"
+                                    aria-controls="flush-collapseFour">
+                                If the domain I’m leasing was used for another website before, how do I handle that
+                                traffic? How does this impact SEO?
+                            </button>
+                        </h2>
+                        <div id="flush-collapseFive" class="accordion-collapse collapse"
+                             aria-labelledby="flush-headingFive" data-bs-parent="#accordionFlushExample">
+                            <div class="accordion-body">
+                                <p>Free traffic is usually a good thing. We’ll leave it up to
+                                    you how you want to handle traffic who’s looking for the prior tenant.</p>
+
+                                <p>With SEO, you should be in a better position than just starting from scratch, unless
+                                    the prior tenant “damaged” the property. SEO principles apply the same here as
+                                    anywhere, so due diligence on your end matters.</p>
+                            </div>
+                        </div>
+                    </div>
+                    {{--                    Five--}}
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="flush-headingFive">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                    data-bs-target="#flush-collapseFive" aria-expanded="false"
+                                    aria-controls="flush-collapseFive">
+                                What happens if I decide to change web hosting after putting work into the site?
+                            </button>
+                        </h2>
+                        <div id="flush-collapseFive" class="accordion-collapse collapse"
+                             aria-labelledby="flush-headingFive" data-bs-parent="#accordionFlushExample">
+                            <div class="accordion-body">
+                                <p>You can move your site where you want, let your customers and Google know, and ask
+                                    websites with backlinks to your site to make changes. In the end, you may have some
+                                    residual loss.</p>
+                            </div>
+                        </div>
+                    </div>
+                    {{--                    Six--}}
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="flush-headingSix">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                    data-bs-target="#flush-collapseSix" aria-expanded="false"
+                                    aria-controls="flush-collapseSix">
+                                I want to lease my domain on Identitius. What do I do?
+                            </button>
+                        </h2>
+                        <div id="flush-collapseSix" class="accordion-collapse collapse"
+                             aria-labelledby="flush-headingSix" data-bs-parent="#accordionFlushExample">
+                            <div class="accordion-body">
+                                <p>You can move your site where you want, let your customers and Google know, and ask
+                                    websites with backlinks to your site to make changes. In the end, you may have some
+                                    residual loss.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-         </div>
-      </div>
-   </div>
-</div>
+        </div>
+    </div>
 @endsection
