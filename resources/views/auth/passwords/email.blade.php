981bdc38ca5cf08e13f97bc6ee72b98193ed9fa4
unknown <hungerforcode@gmail.com>
2021-05-13 18:09:03 +0530

    password reset fixed

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.app')
-
 @section('content')
-<div class="container">
+<div class="container mt-10" style="margin-top: 10%">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -31,7 +30,7 @@
                             </div>
                         </div>
 
-                        <div class="form-group row mb-0">
+                        <div class="form-group row mb-0 mt-2">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Send Password Reset Link') }}
