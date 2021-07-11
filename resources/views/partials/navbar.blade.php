e17fa7036ee6ebe52a058edf563689a12ea0cc9f
David Diomede <david@d2creativedesign.com>
2021-07-11 14:35:52 -0700

    Consolidate menu items

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -20,24 +20,31 @@
                 <li class="nav-item">
                     <a class="nav-link" href="{{route('domains')}}">Domains</a></li>
                 <li class="nav-item">
-                    <a class="nav-link cart_items" href="javascript:void(0)" target="_self">Checkout</a>
+                    <a class="nav-link cart_items" href="javascript:void(0)" target="_self">Checkout <i class="fas fa-shopping-cart"></i></a>
                 <li class="nav-item" href="">
                     <a class="nav-link" href="{{route('qa')}}">Q&As</a>
                 </li>
 
                 <!-- Authentication Links -->
                 @guest
-                    @if (Route::has('login'))
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                        </li>
-                    @endif
+                    <div class="dropdown" aria-label="Login/Register">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+                            <svg width="32" height="32" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path stroke-linejoin="round" stroke-linecap="round" stroke-miterlimit="10" stroke="#fff" d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10z"/><path stroke-linecap="round" stroke-miterlimit="10" stroke="#fff" d="M19.33 18.79A8.002 8.002 0 0012 14c-3.28 0-6.09 1.97-7.33 4.79C6.5 20.76 9.1 22 12 22c2.9 0 5.5-1.24 7.33-3.21z"/><path stroke-linecap="round" stroke-miterlimit="10" stroke="#fff" d="M12 14a4 4 0 100-8 4 4 0 000 8z"/></svg>
+                        </a>
 
-                    @if (Route::has('register'))
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                        </li>
-                    @endif
+                        <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                            @if (Route::has('login'))
+                                <li>
+                                    <a class="dropdown-item" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                </li>
+                            @endif
+                                @if (Route::has('register'))
+                                    <li>
+                                        <a class="dropdown-item" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    </li>
+                                @endif
+                        </ul>
+                    </div>
                 @else
                     <li class="nav-item dropdown">
                         <a id="navbarDropdown" class="nav-link dropdown-toggle btn btn-secondary text-white" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
@@ -54,7 +61,7 @@
                             </form>
                             @if(Auth::user()->admin == 1)
                                 <a class="dropdown-item" href="{{ route('admin.dashboard') }}">Dashboard</a>
-                            @else 
+                            @else
                                 <a class="dropdown-item" href="{{ route('user.profile') }}">Profile</a>
                             @endif
                         </div>
