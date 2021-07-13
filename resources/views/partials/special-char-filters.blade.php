311e2fc37c1ee555380c43761b9248609bbf6c41
David Diomede <david@d2creativedesign.com>
2021-07-13 12:29:54 -0700

    Remove and cleanup filter formatting

---
+++ resources/views/partials/special-char-filters.blade.php
@@ -0,0 +1,21 @@
+<div class="col-lg-12 col-sm-12 my-2">
+    <label class="label">Special Characters</label>
+    <div class="row">
+        <div class="col-lg-3 mt-2">
+            <input type="checkbox" class="form-check-input numerals"
+                   name="include_domains_with_hyphens" value="" checked>Include domains with hyphens
+        </div>
+        <div class="col-lg-3">
+            <input type="checkbox" class="form-check-input numerals"
+                   name="include_domains_with_numerals" value="" checked>Include domains with numerals
+        </div>
+        <div class="col-lg-3">
+            <input type="checkbox" class="form-check-input numerals" name="include_idn_domains"
+                   value="" checked>Include IDN domains
+        </div>
+        <div class="col-lg-3">
+            <input type="checkbox" class="form-check-input numeralsonly"
+                   name="domains_with_numerals_only" value="">Domains with numerals only
+        </div>
+    </div>
+</div>
