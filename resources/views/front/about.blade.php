01f73778178113a18a8b7b091633bf7861fafa3b
David Diomede <david@d2creativedesign.com>
2021-07-13 10:05:10 -0700

    Formatting

--- resources/views/front/about.blade.php
+++ resources/views/front/about.blade.php
@@ -1,39 +1,58 @@
 @extends('layouts.app')
 @section('content')
-<div class="container-fluid container-white mb-neg">
-    <div class="container">
-        <div class="col">
-            <h1 class="display-4 text-center">
-               About Us
-            </h1>
-            <h2>Claim your credible domain, hassle-free.</h2>
-
-            <p>If you’re a startup or entrepreneur, you’ve likely been in that place we call the Domain Dump. You have the perfect domain in mind, but it’s already taken—and even worse, it’s sitting there unused.</p>
-
-            <p>Buying the domain outright feels risky when the future is uncertain…</p>
-
-            <p>Meanwhile, successfully reaching the domain owner with your offer is easier said than done. So, you move from “.com” down to obscurity, or you decide you’ll have to change your name altogether.
-
-            <p>What if you didn’t have to go back to the drawing board?</p>
-
-            <p>Identitius is your affordable solution: Lease or buy the domain you’re looking for, in a hassle-free platform that connects you directly to the domain owner.</p>
-
-            <h5>Through an Identitius domain lease, you can:</h5>
-            <ul class="checkmark">
-                <li>✔  Snag a top-level domain at a fair price. Use a top-level domain through a fair monthly plan.</li>
-                <li>✔  Reduce the financial risk that comes with buying a domain outright.</li>
-                <li>✔  Kick-start your business and focus on what really matters to get it up and running.</li>
-                <li>✔  Have control of your site development and design.</li>
-                <li> ✔  Negotiate lease or purchase terms with the domain owner.</li>
-            </ul>
-
-            <p>You’ll have the option to buy the domain at any point during the lease, at the price you secured up front.</p>
-
-            <p>Domain owners are often waiting for a big pay day that never comes. Identitius bridges this gap to deliver a win-win for both parties.</p>
-
-            <p>With Identitius, you can secure the domain you want to launch your brand in the right direction.</p>
-
+    <div class="row mt-6">
+        <div class="col-sm-12">
+            <h1 class="text-theme text-center">Questions &amp; Answers</h1>
+            <div class="separator-3"></div>
+            <hr>
         </div>
     </div>
-</div>
+    <div class="container-fluid container-white mb-neg">
+        <div class="container">
+            <div class="row mt-6">
+                <div class="col">
+                    <h1 class="text-theme text-center">About Us</h1>
+                    <div class="separator-3"></div>
+                    <hr>
+                </div>
+            </div>
+            <div class="row"></div>
+            <div class="col">
+                <h2>Claim your credible domain, hassle-free.</h2>
+
+                <p>If you’re a startup or entrepreneur, you’ve likely been in that place we call the Domain Dump. You
+                    have the perfect domain in mind, but it’s already taken—and even worse, it’s sitting there
+                    unused.</p>
+
+                <p>Buying the domain outright feels risky when the future is uncertain…</p>
+
+                <p>Meanwhile, successfully reaching the domain owner with your offer is easier said than done. So, you
+                    move from “.com” down to obscurity, or you decide you’ll have to change your name altogether.
+
+                <p>What if you didn’t have to go back to the drawing board?</p>
+
+                <p>Identitius is your affordable solution: Lease or buy the domain you’re looking for, in a hassle-free
+                    platform that connects you directly to the domain owner.</p>
+
+                <h5>Through an Identitius domain lease, you can:</h5>
+                <ul class="checkmark">
+                    <li>Snag a top-level domain at a fair price. Use a top-level domain through a fair monthly plan.
+                    </li>
+                    <li>Reduce the financial risk that comes with buying a domain outright.</li>
+                    <li>Kick-start your business and focus on what really matters to get it up and running.</li>
+                    <li>Have control of your site development and design.</li>
+                    <li> Negotiate lease or purchase terms with the domain owner.</li>
+                </ul>
+
+                <p>You’ll have the option to buy the domain at any point during the lease, at the price you secured up
+                    front.</p>
+
+                <p>Domain owners are often waiting for a big pay day that never comes. Identitius bridges this gap to
+                    deliver a win-win for both parties.</p>
+
+                <p>With Identitius, you can secure the domain you want to launch your brand in the right direction.</p>
+
+            </div>
+        </div>
+    </div>
 @endsection
