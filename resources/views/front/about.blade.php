8dfa3fff9bd88dbf90d7a8f1d194c94301f963e1
David Diomede <david@d2creativedesign.com>
2021-07-14 10:00:47 -0700

    Formatting fixes

--- resources/views/front/about.blade.php
+++ resources/views/front/about.blade.php
@@ -1,16 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
-        <div class="container">
-            <div class="row mt-6">
-                <div class="col">
-                    <h1 class="text-theme text-center">About Us</h1>
-                    <div class="separator-3"></div>
-                    <hr>
-                </div>
+    <div class="container">
+        <div class="row mt-6">
+            <div class="col">
+                <h1 class="text-theme text-center">About Us</h1>
+                <div class="separator-3"></div>
+                <hr>
             </div>
-            <div class="row">
-                <div class="col-lg-8 col-sm-12 mx-auto">
+        </div>
+        <div class="row">
+            <div class="col-lg-8 col-sm-12 mx-auto">
                 <h2>Claim your credible domain, hassle-free.</h2>
 
                 <p>If you’re a startup or entrepreneur, you’ve likely been in that place we call the Domain Dump. You
@@ -36,19 +36,16 @@
                     <li> Negotiate lease or purchase terms with the domain owner.</li>
                 </ul>
 
-                <p>You’ll have the option to <strong>buy the domain at any point</strong> during the lease, at the price you secured up
+                <p>You’ll have the option to <strong>buy the domain at any point</strong> during the lease, at the price
+                    you secured up
                     front.</p>
 
                 <p>Domain owners are often waiting for a big pay day that never comes. Identitius bridges this gap to
                     deliver a win-win for both parties.</p>
 
                 <p>With Identitius, you can secure the domain you want to launch your brand in the right direction.</p>
-            </div>
-            </div>
-            <div class="row">
-                <div class="col">
-                    <a href="/domains" class="btn btn-primary">FIND YOUR DOMAIN TODAY!</a>
-                </div>
+                <a href="/domains" class="btn btn-primary">FIND YOUR DOMAIN TODAY!</a>
             </div>
         </div>
+    </div>
 @endsection
