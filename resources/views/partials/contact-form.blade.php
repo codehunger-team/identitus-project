e48cab548219c62234c79dabb0e03f93751a9a7a
David Diomede <david@d2creativedesign.com>
2021-09-30 11:06:23 -0700

    Footer

--- resources/views/partials/contact-form.blade.php
+++ resources/views/partials/contact-form.blade.php
@@ -1,34 +1,17 @@
 <form class="py-4" method="post" action="{{route('send.enquiry')}}">
    @csrf
-   <div class="form-group">
-       <label for="name">Name <span class="text-danger">*</span></label>
-       <input type="name" name="name" class="form-control  @error('name') is-invalid @enderror" required id="name" aria-describedby="name"
-           placeholder="Enter Name">
-       @error('name')
-       <span class="invalid-feedback" role="alert">
-           <strong>{{ $message }}</strong>
-       </span>
-       @enderror
-   </div>
-   <div class="form-group">
-       <label for="email">Email <span class="text-danger">*</span></label>
-       <input type="email" name="email" class="form-control  @error('email') is-invalid @enderror" required id="email" aria-describedby="email"
-           placeholder="Enter email">
-       @error('email')
-       <span class="invalid-feedback" role="alert">
-           <strong>{{ $message }}</strong>
-       </span>
-       @enderror
-   </div>
-   <div class="form-group">
-       <label for="message" class="form-label">Your Message <span class="text-danger">*</span></label>
-       <textarea class="form-control" name="message" required id="exampleFormControlTextarea1"
-           placeholder="I want to know ..." rows="3"></textarea>
-       @error('message')
-       <span class="invalid-feedback" role="alert">
-           <strong>{{ $message }}</strong>
-       </span>
-       @enderror
-   </div>
+    <div class="form-floating mb-3">
+        <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" aria-describedby="name"
+               placeholder="Full Name" required>
+        <label for="name">Full Name</label>
+    </div>
+    <div class="form-floating mb-3">
+        <input type="email" class="form-control @error('email') is-invalid @enderror" id="email" placeholder="name@example.com" required>
+        <label for="email">Email Address</label>
+    </div>
+    <div class="form-floating">
+        <textarea class="form-control @error('contactMessage') is-invalid @enderror" placeholder="Leave a comment here" id="contactMessage" style="height: 100px" required></textarea>
+        <label for="contactMessage" >Comments</label>
+    </div>
    <button type="submit" class="btn btn-warning my-2 float-end">Submit</button>
 </form>
