1e8d41b1e061a76de078c0d9c7d1071aff477563
David Diomede <david@d2creativedesign.com>
2021-12-08 09:04:55 -0700

    Recaptcha 3

--- resources/views/partials/contact-form.blade.php
+++ resources/views/partials/contact-form.blade.php
@@ -1,4 +1,4 @@
-<form class="py-4" method="post" action="{{route('send.enquiry')}}">
+<form id="biscolab-recaptcha-invisible-form" class="py-4" method="post" action="{{route('send.enquiry')}}">
    @csrf
     <div class="form-floating mb-3">
         <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" name="name" aria-describedby="name"
@@ -11,7 +11,7 @@
     </div>
     <div class="form-floating">
         <textarea class="form-control @error('contactMessage') is-invalid @enderror" name="message" placeholder="Leave a comment here" id="contactMessage" style="height: 100px" required></textarea>
-        <label for="contactMessage" >Comments</label>
+        <label for="contactMessage">Comments</label>
     </div>
    <button type="submit" class="btn btn-warning my-2 float-end">Submit</button>
 </form>
