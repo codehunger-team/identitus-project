aa2d34114772e357effd2c734d5b1ce8667286d0
unknown <hungerforcode@gmail.com>
2023-04-14 00:41:18 +0530

    minor change towards order

--- resources/views/user/lessee/view-order.blade.php
+++ resources/views/user/lessee/view-order.blade.php
@@ -57,6 +57,54 @@
 </div>
 @endsection
 @section('section_body')
+<div class="row">
+	<div class="col-xs-12 col-md-4">
+		<div class="box">
+			<div class="box-header with-border"><strong>Order Contact</strong></div>
+			<div class="box-body">
+
+			<dl>
+			<dt>Customer Name:</dt>
+				<dd>{{ $order->customer }}</dd>
+			<dt>Customer Email:</dt>
+			<dd>
+				<a href="mailto:{{ $order->email }}?subject=Order #{{ $order->id }}">
+					{{ $order->email }}
+				</a>
+			</dd>
+			</dl>
+
+			</div>
+		</div>
+	</div>
+	<div class="col-xs-12 col-md-8">
+		<div class="box">
+			<div class="box-header with-border"><strong>Order Info</strong></div>
+			<div class="box-body">
+			
+			<div class="row">
+				<div class="col-xs-6">
+					<dl>
+					<dt>Order Status</dt>
+						<dd>{{ $order->order_status}}</dd>
+					<dt>Total</dt>
+						<dd>${{ number_format( $order->total, 0 ) }}</dd>
+					</dl>
+				</div><!-- ./col-xs-6 -->
+				<div class="col-xs-6">
+					<dl>
+					<dt>Order Date</dt>
+						<dd>{{app('App\Helpers\DateTimeHelper')->ConvertIntoUTC($order->order_date)}} </dd>
+					<dt>Payment Type</dt>
+						<dd>{{ $order->payment_type }}</dd>
+					</dl>
+				</div><!-- ./col-xs-6 -->
+			</div><!-- ./row -->
+
+			</div>
+		</div>
+	</div>
+</div>
 
 	<table class="table dataTable">
 	<thead>
