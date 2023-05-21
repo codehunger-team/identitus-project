6295a8403c76e58f38693c965c9dc4ad2573a1a8
unknown <hungerforcode@gmail.com>
2023-05-21 16:33:34 +0530

    added the code for stripe connect

--- resources/views/user/lessee/become-vendor.blade.php
+++ resources/views/user/lessee/become-vendor.blade.php
@@ -6,15 +6,15 @@
 
 @section('section_body')
 @if(Session::has('docusign'))
-    @php
-    $clickwrap = Session::get('docusign');
-    @endphp
-    @include('user.lessee.docusign.become-vendor-terms')
+@php
+$clickwrap = Session::get('docusign');
+@endphp
+@include('user.lessee.docusign.become-vendor-terms')
 @endif
 
 @if(Auth::user()->is_vendor == 'pending')
 <div class="alert alert-warning" role="alert">
-    <h5>Thank you, {{Auth::user()->name}} for your interest in becoming a vendor. Your application is being reviewed. 
+    <h5>Thank you, {{Auth::user()->name}} for your interest in becoming a vendor. Your application is being reviewed.
         Once approved, you will receive an email confirmation. You can also check here periodically.</h5>
 </div>
 
@@ -23,19 +23,31 @@
     <h5>Your profile has been successfully verified. Now you can start adding your domains. Good luck!</h5>
 </div>
 @else
-<div class="alert alert-danger" role="alert">
-    <h5>Hello {{Auth::user()->name}}, We are happy to see your interest in being a Domain Lessor and Seller. You are one step away from listing domains on our website.
-        Please click on the below button to register yourself or your company as a vendor (Lessor) and start listing your domains.
-        It can take 24 to 48 hours to pass the verification process.</h5>
-</div>
-<a href="{{route('sign.document','terms.pdf')}}" id="become-vendor" class="btn btn-primary btn-block text-center">Become
-    Vendor</a>
+<section class="my-4 pb-2 border-bottom ">
+
+    <div class="alert alert-danger" role="alert">
+        <h5>In order to receive money you must have to connect your stripe account with us.</h5>
+    </div>
+    <a href="{{route('user.stripe-connect')}}" id="connet_stripe" class="btn btn-primary btn-block text-center">Connect
+        Account</a>
+</section>
+<section>
+    <div class="alert alert-danger" role="alert">
+        <h5>Hello {{Auth::user()->name}}, We are happy to see your interest in being a Domain Lessor and Seller. You are
+            one step away from listing domains on our website.
+            Please click on the below button to register yourself or your company as a vendor (Lessor) and start listing
+            your domains.
+            It can take 24 to 48 hours to pass the verification process.</h5>
+    </div>
+    <a href="{{route('sign.document','terms.pdf')}}" id="become-vendor"
+        class="btn btn-primary btn-block text-center">Become
+        Vendor</a>
+</section>
 <script>
     $(document).on('click', '#become-vendor', function () {
         $(this).addClass('disabled');
         $(this).text('Redirecting to terms page ...')
     });
-
 </script>
 @endif
 @endsection
