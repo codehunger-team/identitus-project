af33f24d1f96bc62cf0ef0f90e421c5a3cdf6055
seamarhay <smh@seanhays.com>
2023-06-12 09:40:01 -0700

    new page added again

--- resources/views/front/fees.blade.php
+++ resources/views/front/fees.blade.php
@@ -11,7 +11,30 @@
     <div class="row">
         <div class="col-lg-8 col-sm-12 mx-auto">
             <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            At Identitius.com, we provide a secure and convenient platform for domain owners and renters to connect and transact. To ensure the smooth functioning of our services, we have established the following fees:
+
+1. Domain Renters:
+
+Domain renters are individuals or businesses who wish to lease a domain name from our platform. As a domain renter, you are responsible for the following fees:
+
+Monthly Rental Fee: The monthly rental fee is the cost associated with leasing a domain name from one of our registered domain owners. The specific rental fee for each domain is determined by the domain owner and can vary based on factors such as domain popularity, length, and market demand.
+
+Credit Card Processing Fees: In addition to the monthly rental fee, domain renters are required to cover the associated credit card processing fees. These fees are incurred when making payments through our platform and are charged by the payment gateway service provider. The exact percentage or amount of the credit card processing fees may vary and is determined by the payment gateway provider.
+
+2. Domain Owners:
+
+Domain owners are individuals or businesses who list their domain names for rent or sale on Identitius.com. As a domain owner, you are subject to the following fees:
+
+Rental Commission: When a domain owner successfully rents out their domain through our platform, Identitius.com charges a commission equal to 20% of the rents received. This commission is deducted from the total rental amount and is payable to Identitius.com.
+
+Domain Sales Commission: In the event that a domain owner sells their domain through Identitius.com, a commission of 20% of the sale price is applicable. This commission is calculated based on the final sale price and is payable to Identitius.com.
+
+Please note that all fees and commissions mentioned above are subject to applicable taxes and fees imposed by regulatory authorities. The exact fees and commissions will be clearly displayed and communicated to you during the transaction process on Identitius.com.
+
+We strive to maintain transparency and fairness in our fee structure, and the fees collected contribute to the continuous improvement and development of our platform, ensuring a reliable and secure domain rental marketplace for our users.
+
+Should you have any further questions or require additional clarification regarding our fees, please do not hesitate to contact our customer support team, who will be more than happy to assist you.
+                
             </p>
         </div>
     </div>
