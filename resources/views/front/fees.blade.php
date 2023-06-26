c008702f70a47ea8b0817b839018b69713303fe7
Sean <github@seanhays.com>
2023-06-26 12:32:01 -0700

    change in fees page

--- resources/views/front/fees.blade.php
+++ resources/views/front/fees.blade.php
@@ -3,28 +3,32 @@
 <div class="container">
     <div class="row mt-6">
         <div class="col">
-            <h1 class="text-theme text-center">Fees</h1>
+            <h1 class="text-theme text-center">Identitius Fees and Charges</h1>
             <div class="separator-3"></div>
             <hr>
         </div>
     </div>
     <div class="row">
         <div class="col-lg-8 col-sm-12 mx-auto">
-                <title>Identitius - Fees</title>
-                <h1>Fees</h1>
-                <h2>Domain Rental Fees</h2>
-                <p>Domain  on Identitius are required to pay a monthly rental fee for the domain they wish to use. In addition to the rental fee, there are associated transaction (credit card) processing fees.</p>
-                <p>Please note that the exact rental fee and processing fees may vary depending on the specific domain and rental terms.</p>
+                <title>Identitius Fees</title>
 
-                <h2>Domain Ownership Fees</h2>
-                <p>Domain owners on Identitius are subject to the following fees:</p>
+                <h2>For the Renter or Leasee:</h2>
+                <p>Identitius does not charge domain renters or leasees directly for any services. 
+                    
+                <p>When you lease a domain through Identitius, we collect both the rent and the payment processing (credit card) fee. The payment processing fees never hit our account, and the rent is passed directly on to the domain owner / lessor minus our fee to them. Our goal is to lower our fees as much as we can, but there is a real cost to maintaining these services for you and others. We thank you for using Identitius and for helping us to these services possible for everyone.</p>
+                
+                <p>As you probably know, the exact rent and payment processing fees are based on the specific domain and rental terms.</p>
+
+                <h2>For the Domain Owner or Lessor:</h2>
+                <p>Domain owners / lessors on Identitius are subject to the following fees:</p>
                 <ul>
-                    <li><strong>Commission on Rents Received:</strong> Domain owners pay a 20% commission on the rents they receive from domain .</li>
-                    <li><strong>Commission on Domain Sales:</strong> In the event of a domain sale, domain owners are required to pay a 20% commission based on the final sale price.</li>
+                    <li><strong>Commission on Rents Received:</strong> Domain owners pay a 10% commission on domains pointed at Identitius.com and a 20% commission on domains not pointed at Identitius.com.</li>
+                    <li><strong>Commission on Domain Sales:</strong> In the event of a domain sale, domain owners pay a 10% commission on domains pointed at Identitius.com and a 20% commission on domains not pointed at Identitius.com.</li>
+                    <li><strong>All payment processing fees are charged to the customer seperately and passed on directly to the payment processing company: Stripe.
                 </ul>
 
                 <h3>Disclaimer</h3>
-                <p>Please note that the fees mentioned above are subject to change and may vary based on individual agreements and circumstances. Identitius reserves the right to modify the fees at any time. It is recommended to review the specific terms and conditions associated with each domain for accurate fee information.</p>
+                <p>Please note that the fees mentioned above are subject to change and may vary based on individual agreements and circumstances. Identitius reserves the right to modify the fees at any time. It is recommended to review the specific terms and conditions associated with each domain for accurate fee information and check here regularly for future fees and charges on domain leases and sales.</p>
         </div>
     </div>
 </div>
