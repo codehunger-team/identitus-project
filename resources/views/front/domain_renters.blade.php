7a5de298017008981d81e4b5b1493362a0ec695a
Sean <github@seanhays.com>
2023-06-26 22:12:27 -0700

    Redesign new menu pages

--- resources/views/front/domain_renters.blade.php
+++ resources/views/front/domain_renters.blade.php
@@ -10,9 +10,9 @@
     </div>
     <div class="row">
         <div class="col-lg-8 col-sm-12 mx-auto">
-                <h1>Advantages of Renting a Domain Name with Option to Buy</h1>
+                <h1>Advantages of Renting a Domain Name<br>
+                with Option to Buy</h1>
                 
-                <h2>Unlocking Opportunities for Your Business</h2>
                 <p>Renting a domain name with an option to buy offers significant advantages for your business or project. This unique arrangement combines the benefits of immediate domain access with the potential for long-term ownership. Let's explore the advantages:</p>
                 
                 <h3>1. Test and Validate</h3>
@@ -22,18 +22,18 @@
                 <p>Renting a domain name significantly reduces the upfront financial risk associated with domain ownership. Instead of investing a substantial amount upfront, you can start using the domain immediately at a fraction of the cost. This allows you to allocate your resources to other aspects of your business while still benefiting from a premium web address.</p>
                 
                 <h3>3. Flexibility and Scalability</h3>
-                <p>Domain leasing with an option to buy provides you with flexibility and scalability. You have the freedom to choose the rental duration that suits your specific needs, whether it's a short-term campaign or a long-term venture. Additionally, as your business grows, the option to buy gives you the ability to secure the domain and scale your online presence accordingly.</p>
+                <p>Domain leasing with an option to buy provides you with flexibility and scalability. You have the freedom to work with the domain owner to choose the rental duration that suits your specific needs, whether it's a short-term campaign or a long-term venture. Additionally, as your business grows, the option to buy gives you the ability to secure the domain and scale your online presence accordingly.</p>
                 
                 <h3>4. Protection and Security</h3>
-                <p>The option to buy clause in the lease agreement offers you protection and security. It ensures that if the domain proves to be integral to your business success, you have the exclusive right to purchase it within the agreed timeframe and at a pre-determined price. This eliminates the risk of losing the domain to competitors and provides you with a sense of security for your online brand identity.</p>
+                <p>The option to buy clause in the lease agreement offers you protection and security. This ensures that if the domain proves to be integral to your business success, you have the right to purchase it within the agreed timeframe and at a pre-determined price. This eliminates the risk of losing the domain to competitors and provides you with a sense of security for your online brand identity.</p>
                 
                 <h3>5. Negotiation Power</h3>
-                <p>Having the option to buy the domain grants you increased negotiation power. It demonstrates your commitment and seriousness to the domain owner, allowing you to discuss favorable terms and potentially secure a better deal. The option to buy acts as leverage, fostering a positive relationship with the domain owner throughout the lease period.</p>
+                <p>Having the option to buy the domain grants you increased negotiation power. It demonstrates your commitment and seriousness to the domain owner, allowing you to discuss favorable terms and potentially secure a better deal on lease renewals. The option to buy acts as leverage, fostering a positive relationship with the domain owner throughout the lease period.</p>
                 
-                <h2>Start Your Journey Today</h2>
-                <p>At Identitius.com, we offer a platform that connects you with domain owners who provide the option to lease their domains with the potential to buy. Take advantage of this opportunity to access premium domains, validate their suitability for your business, and secure long-term ownership.</p>
+                <h2>Start Renting a Domain Today</h2>
+                <p>At <a href="https://www.identitius.com">Identitius</a>, we offer a platform that connects you with domain owners who provide the option to lease their domains with the potential to buy. Take advantage of this opportunity to access a premium domain, validate its suitability for your business, and secure long-term ownership.</p>
                 
-                <p>Don't miss out on the advantages of renting a domain name with an option to buy. Join Identitius.com now and embark on a journey that unlocks endless possibilities for your business!</p>
+                <p>Don't miss out on the advantages of renting a domain name with an option to buy. Join <a href="https://www.identitius.com">Identitius</a> now and embark on a journey that unlocks endless possibilities for your business!</p>
         </div>
     </div>
 </div>
