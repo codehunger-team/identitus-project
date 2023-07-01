999a18ea4eb333490c320ddc810eb03999c240b7
unknown <hungerforcode@gmail.com>
2023-07-01 22:52:09 +0530

    datatable issue

--- resources/views/user/lessor/lease/active.blade.php
+++ resources/views/user/lessor/lease/active.blade.php
@@ -78,13 +78,15 @@
             <td>
 				{{ $d->number_of_periods }}
             </td>
-            @foreach($gracePeriod as $key => $gracePeriodValue)
+			<td>5</td>
+            {{-- @foreach($gracePeriod as $key => $gracePeriodValue)
                 @if($gracePeriodValue->id == $d->grace_period_id)
                     <td>
                         {{ $gracePeriodValue->grace_period }}
                     </td>
+                @else
                 @endif
-            @endforeach
+            @endforeach --}}
             <td>
 				{{app('App\Helpers\DateTimeHelper')->ConvertIntoUTC($d->start_date)}} 
             </td>
