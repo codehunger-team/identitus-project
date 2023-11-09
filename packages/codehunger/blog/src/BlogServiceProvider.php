faba14d8fc1ac07fb7669fbb6d53d67d4698eb5d
Sachin Bharmoria <sachin.bharmoria@codehunger.in>
2023-11-09 19:37:07 +0530

    small

--- packages/codehunger/blog/src/BlogServiceProvider.php
+++ packages/codehunger/blog/src/BlogServiceProvider.php
@@ -8,9 +8,9 @@ class BlogServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->loadRoutesFrom(base_path('packages\codehunger\blog\src\Routes\web.php'));
-        $this->loadMigrationsFrom(base_path('packages\codehunger\blog\src\Database\Migrations'));
-        $this->loadViewsFrom(base_path('packages\codehunger\blog\src\Resources\views'), 'blog');
+        $this->loadRoutesFrom(base_path('packages/codehunger/blog/src/Routes/web.php'));
+        $this->loadMigrationsFrom(base_path('packages/codehunger/blog/src/Database/Migrations'));
+        $this->loadViewsFrom(base_path('packages/codehunger/blog/src/Resources/views'), 'blog');
     }
 
     public function register()
